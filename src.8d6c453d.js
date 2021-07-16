84e23e80b5ed101ddf8304d22bd7fd5c5912b492
Oleksandra-Mas <72225437+Oleksandra-Mas@users.noreply.github.com>
2021-07-16 08:58:04 +0000

    Deploying to gh-pages from @ Oleksandra-Mas/ice-cream3@031d2ac46f7ab60dae2750082ac7f6778f027267 🚀

--- src.8d6c453d.js
+++ src.8d6c453d.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-cream3/src.2232d50c.js.map
+//# sourceMappingURL=/ice-cream3/src.8d6c453d.js.map

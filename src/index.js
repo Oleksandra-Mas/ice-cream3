d27b7720f33cab865f9ffbce411fdc5cd51a70ca
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2021-07-22 03:53:40 +0300

    fixed menu

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import './sass/main.scss';
   const closeMenuLink3 = document.querySelector('.js-close-menu-link-3');
   const closeMenuLink4 = document.querySelector('.js-close-menu-link-4');
   const closeMenuLink5 = document.querySelector('.js-close-menu-link-5');
+  const closeMenuButton = document.querySelector('.js-close-menu-button');
+  
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -29,6 +31,7 @@ import './sass/main.scss';
   closeMenuLink3.addEventListener('click', toggleMenu);
   closeMenuLink4.addEventListener('click', toggleMenu);    
   closeMenuLink5.addEventListener('click', toggleMenu);
+  closeMenuButton.addEventListener('click', toggleMenu);
 
   window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
     if (!e.matches) return;
